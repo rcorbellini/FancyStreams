c2ba80882d11d71955936f9d85cf141ac1f345d6
Rafael Corbellini <rafael.corbellini@egsys.com.br>
2020-08-11 16:13:14 -0300

    fix: format

--- example/example.dart
+++ example/example.dart
@@ -1,12 +1,10 @@
 import 'package:fancy_stream/fancy_stream.dart';
 
 class Example extends Disposable {
-
-  void main(List<String> args) {    
+  void main(List<String> args) {
     //listen something on String, with key = "print"
-    listenOn<String>(printWhenDispatchedValue, key:"print");
+    listenOn<String>(printWhenDispatchedValue, key: "print");
     dispatchOn<String>("Print that!", key: "print");
-
 
     listenOn<String>(printWhenDispatchedValue);
     dispatchOn<String>("Print that without key!");
@@ -16,7 +14,7 @@ class Example extends Disposable {
 
     ///Clean all Subjects, Subscrtiption and instances generated
     dispose();
-  } 
+  }
 
   void printWhenDispatchedValue(String value) {
     print(value);
--- lib/src/disposable.dart
+++ lib/src/disposable.dart
@@ -3,10 +3,9 @@ import 'fancy_stream_ext.dart';
 
 ///A abstraction of  something can be disposable, everything can be disosable
 /// has all of power of fancy stream (extesion functions over Disposable).
-/// This class only have a [dispose] function, when you no need more 
-/// yours streams you MUST call it. 
+/// This class only have a [dispose] function, when you no need more
+/// yours streams you MUST call it.
 abstract class Disposable {
-
   ///Must call when your imlpementation of this class are being disposed.
   @mustCallSuper
   void dispose() {
